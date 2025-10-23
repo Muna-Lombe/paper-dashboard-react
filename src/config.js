aa1f5d4df410669eaf6f1abe452cb702ccf7bcd7
MunaLombe <3223352545@qq.com>
2025-10-24 02:55:48 +0300

    configured server-url

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 // const baseApiUrl = "https://de885a4b-d886-4a17-9372-6791449191cc-00-sk4u39m5xe4b.picard.replit.dev:5000"//"https://paper-dash-api.onrender.com";
 
-import axios from "axios";
-axios.defaults.withCredentials = true;
 
 // const baseApiUrl = "https://paper-dash-api.onrender.com"
-const baseApiUrl = "http://localhost:5000"
+const baseApiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000"
 axios.defaults.withCredentials = true;
 
 export const endpoints = {
