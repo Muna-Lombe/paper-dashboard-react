7e85412e5fab684be279e1bc4a347ef08a0133c5
MunaLombe <3223352545@qq.com>
2025-10-29 12:40:01 +0300

    ...

--- index.js
+++ index.js
@@ -115,3 +115,5 @@ server.listen(PORT, () => {
     // // // console.log(`Server running on port ${PORT}`);
     // // // console.log(`API Documentation available at http://localhost:${PORT}/api-docs`);
 });
+
+// export default { ... }
